0abf31cb1a6d7ec7a09b36874840808d796ac963
Minhaj Sadik <MinhajoSadik@gmail.comm>
2021-06-13 21:53:15 +0600

    convert a birthday appwith react

--- src/App.js
+++ src/App.js
@@ -1,17 +1,39 @@
 import MessengerCustomerChat from 'react-messenger-customer-chat';
 import './App.css';
-import logo from './logo.svg';
+import Joynul from './images-videos/joynul.jpg';
+import Nayeem from './images-videos/nayeem.jpg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
+    <div>
+      <marquee>আপনি যার জন্মদিন এর শুভেচ্চা দেখতে চান তার ছবিতে ক্লিক করেন! Have agood day, See You Soon!
+    </marquee>
+      <nav>
+        <ul>
+          <li>Sajon</li>
+          <li>Nayeem</li>
+          <li>Joynul</li>
+        </ul>
+      </nav>
+
+      <div className="group-images">
+      <img alt="Nayeem" src={Nayeem}/>
+      <img alt="Joynul" src={Joynul}/>
+      </div>
+     
+     
+    
+
+
+
+      {/* <nav>
+          <iframe title="_blank" src="https://react-portfolio-resume.netlify.app/"></iframe>
+      </nav> */}
+
     <MessengerCustomerChat
-    pageId="<830505353752858>"
-    appId="<1041336330018020>"
-  />
+    pageId="830505353752858"
+    appId="1041336330018020"
+    />
     </div>
   );
 }
